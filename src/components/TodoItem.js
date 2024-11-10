d2502c81a7237fcc814f52f566c87cca42210765
naffsvn <naffsvn@protonmail.com>
2024-11-10 14:33:14 +0700

    [modify] - styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,20 +1,85 @@
 import React from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-const style = StyleSheet.create({
+function TodoItem({task, deleteTask, toggleCompleted}) {
+  return (
+    <View style={styles.todoItem}>
+      {/* Bagian Kiri: Nama, Harga, dan Total Item */}
+      <View style={styles.leftSection}>
+        <Text style={[styles.todoItemText, task.completed && styles.completed]}>
+          {task.text}
+        </Text>
+        <Text style={[styles.todoItemText, task.completed && styles.completed]}>
+          Harga: {task.textPrice}
+        </Text>
+        <Text style={[styles.todoItemText, task.completed && styles.completed]}>
+          Total: {task.textTotal}
+        </Text>
+      </View>
+
+      {/* Bagian Tengah: Harga x Total Item dan Total Harga */}
+      <View style={styles.centerSection}>
+        <Text style={[styles.todoItemText, task.completed && styles.completed]}>
+          {task.textPrice} x {task.textTotal}
+        </Text>
+        <Text style={[styles.todoItemText, task.completed && styles.completed]}>
+          Total Harga: {task.textPrice * task.textTotal}
+        </Text>
+      </View>
+
+      {/* Bagian Kanan: Tombol */}
+      <View style={styles.rightSection}>
+        <TouchableOpacity
+          style={styles.deleteButton}
+          onPress={() => deleteTask(task.id)}>
+          <Text style={{color: '#fff'}}>Hapus</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={
+            task.completed ? styles.completeButton : styles.uncompleteButton
+          }
+          onPress={() => toggleCompleted(task.id)}>
+          <Text style={{color: '#fff'}}>
+            {task.completed ? 'Selesai' : 'Tertunda'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+export default TodoItem;
+
+const styles = StyleSheet.create({
   todoItem: {
-    display: 'flex',
+    flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     marginBottom: 8,
     padding: 8,
-    border: 1,
+    borderWidth: 1,
     borderColor: '#ddd',
-    borderRadius: 4,
+    borderRadius: 10,
+    backgroundColor: '#fff',
   },
-  todoItemTeks: {
+  leftSection: {
     flex: 1,
-    marginRight: 8,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  centerSection: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rightSection: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  todoItemText: {
+    marginBottom: 4,
     color: '#333',
     fontSize: 16,
   },
@@ -24,44 +89,19 @@ const style = StyleSheet.create({
   },
   deleteButton: {
     backgroundColor: '#D04848',
-    color: '#fff',
     padding: 4,
-    border: 'none',
     borderRadius: 4,
-    cursor: 'pointer',
+  },
+  uncompleteButton: {
+    backgroundColor: '#F3B95F',
+    padding: 4,
+    borderRadius: 4,
+    marginTop: 4,
   },
   completeButton: {
-    backgroundColor: '#F3B95F',
-    color: '#000',
+    backgroundColor: 'green',
     padding: 4,
-    border: 'none',
     borderRadius: 4,
-    cursor: 'pointer',
+    marginTop: 4,
   },
 });
-
-function TodoItem({task, deleteTask, toggleCompleted}) {
-  return (
-    <View style={style.todoItem}>
-      <Text style={(style.todoItemTeks, task.completed && style.completed)}>
-        {task.text}
-      </Text>
-      <TouchableOpacity
-        style={style.deleteButton}
-        onPress={() => deleteTask(task.id)}>
-        <Text style={{color: '#fff'}}>Delete</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={style.completeButton}
-        onPress={() => toggleCompleted(task.id)}>
-        <Text style={{color: '#fff'}}>
-          {task.completed && 'completed'}
-          {!task.completed && 'uncompleted'}
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-
-export default TodoItem;
--- src/screen/TodoList.js
+++ src/screen/TodoList.js
@@ -1,19 +1,67 @@
 import React, {useState} from 'react';
-import {Button, TextInput, View} from 'react-native';
+import {
+  FlatList,
+  TextInput,
+  View,
+  StyleSheet,
+  StatusBar,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import TodoItem from '../components/TodoItem';
+import uuid from 'react-native-uuid';
 
 const TodoList = () => {
   const [tasks, setTasks] = useState([
-    {id: 1, text: 'Buy milk', completed: true},
-    {id: 2, text: 'Buy eggs', completed: false},
+    {
+      id: 1,
+      text: 'Buy milk',
+      textTotal: '1',
+      textPrice: '1000',
+      completed: true,
+    },
+    {
+      id: 2,
+      text: 'Buy eggs',
+      textTotal: '2',
+      textPrice: '2000',
+      completed: false,
+    },
   ]);
 
+  const options = {
+    weekday: 'long',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  };
+
+  const formattedDate = new Intl.DateTimeFormat('id-ID', options).format(
+    new Date(),
+  );
+
   const [text, setText] = useState('');
+  const [textTotal, setTextTotal] = useState('');
+  const [textPrice, setTextPrice] = useState('');
 
   function addTask() {
-    const newTask = {id: Date.now(), text, completed: false};
+    const newTask = {
+      id: uuid.v4(),
+      text,
+      textTotal,
+      textPrice,
+      date: formattedDate,
+      completed: false,
+    };
     setTasks([...tasks, newTask]);
     setText('');
+    setTextTotal('');
+    setTextPrice('');
+    Keyboard.dismiss();
   }
 
   function deleteTask(id) {
@@ -29,18 +77,116 @@ const TodoList = () => {
   }
 
   return (
-    <View>
-      {tasks.map(task => (
-        <TodoItem
-          key={task.id}
-          task={task}
-          deleteTask={deleteTask}
-          toggleCompleted={toggleCompleted}
-        />
-      ))}
-      <TextInput value={text} onChangeText={setText} placeholder="New Task" />
-      <Button title="Add" onPress={addTask} />
-    </View>
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 80 : 0}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <StatusBar hidden />
+          <Text
+            style={{
+              fontSize: 24,
+              fontWeight: 'bold',
+              color: '#fff',
+              textAlign: 'center',
+              paddingVertical: 16,
+            }}>
+            To Do List
+          </Text>
+          <FlatList
+            data={tasks}
+            style={{paddingHorizontal: 16}}
+            renderItem={({item}) => (
+              <TodoItem
+                key={item.id}
+                task={item}
+                deleteTask={deleteTask}
+                toggleCompleted={toggleCompleted}
+              />
+            )}
+            keyExtractor={item => item.id.toString()}
+            contentContainerStyle={{flexGrow: 1}}
+            showsVerticalScrollIndicator={true}
+          />
+          <View style={styles.buttonContainer}>
+            <TextInput
+              value={text}
+              onChangeText={setText}
+              placeholder="Nama Barang"
+              placeholderTextColor="#000"
+              style={styles.input}
+            />
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <TextInput
+                value={textTotal}
+                onChangeText={setTextTotal}
+                placeholder="Total"
+                placeholderTextColor="#000"
+                style={styles.inputAddOn}
+                keyboardType="numeric"
+              />
+              <TextInput
+                value={textPrice}
+                onChangeText={setTextPrice}
+                placeholder="Harga"
+                placeholderTextColor="#000"
+                style={styles.inputAddOn}
+                keyboardType="numeric"
+              />
+            </View>
+            <TouchableOpacity onPress={addTask} style={styles.addButton}>
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#48CAE4',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingLeft: 8,
+    borderRadius: 4,
+  },
+  inputAddOn: {
+    height: 40,
+    width: 150,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingLeft: 8,
+    borderRadius: 4,
+  },
+  buttonContainer: {
+    padding: 16,
+    paddingBottom: 40,
+    backgroundColor: '#fff',
+    borderTopEndRadius: 16,
+    borderTopStartRadius: 16,
+  },
+  addButton: {
+    backgroundColor: '#48CAE4',
+    paddingVertical: 12,
+    marginTop: 16,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
+
 export default TodoList;
